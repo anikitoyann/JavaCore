70d429c1c6adb3b5ae9e13034e7b210ec5900615
Ani <anikitoyan11@gamil.com>
2022-12-21 11:48:32 +0400

    ArrayUtil

--- src/homework/homework2/ArrayUtil.java
+++ src/homework/homework2/ArrayUtil.java
@@ -1,37 +1,37 @@
 package homework.homework2;
 
 
-    public class ArrayUtil {
-        public static void main(String[] args) {
-            int i;
-            int[] array = {44, 22, 5, 8, 10, 32, 77, 11, 66, 12, 30};
-            for (i = 0; i < array.length; i++) {
-                System.out.print(array[i] + " ");
-            }
-            System.out.println();
-
-
-            int min = array[0];
-            for (i = 0; i < array.length; i++) {
-
-                if (array[i] < min) {
-                    min = array[i];
-                }
-            }
-            System.out.print("min = " + min);
-            System.out.println();
-
-
-            int max = array[0];
-            for (i = 0; i < array.length; i++) {
-                if (array[i] > max) {
-                    max = array[i];
-                }
-            }
-            System.out.println("max = " + max);
-
-
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int i;
+        int[] array = {44, 22, 5, 8, 10, 32, 77, 11, 66, 12, 30};
+        for (i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
+        System.out.println();
+
+
+        int min = array[0];
+        for (i = 0; i < array.length; i++) {
+
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.print("min = " + min);
+        System.out.println();
+
+
+        int max = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max = " + max);
+
 
     }
+
+}
 
