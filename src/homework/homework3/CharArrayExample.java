11c89c97151c0d4953ace1bbfc3f846e7ec98e76
Ani <anikitoyan11@gamil.com>
2022-12-29 23:31:58 +0400

    CharArray

--- src/homework/homework3/CharArrayExample.java
+++ src/homework/homework3/CharArrayExample.java
@@ -15,19 +15,24 @@ public class CharArrayExample {
         char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
         System.out.println("middleElement= " + chars2[chars2.length / 2 - 1] + "," + chars2[chars2.length / 2]);
 
-        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
-        boolean char3=false ;
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'k'};
+
+        boolean char3;
         if ((chars3[chars3.length - 2] == 'l') && (chars3[chars3.length - 1] == 'y')) {
             char3 = true;
-            System.out.println(char3);
-
+        } else {
+            char3 = false;
         }
+        System.out.println(char3);
+
         char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
         for (int i = 0; i < text.length; i++) {
             if (text[i] != ' ') {
                 System.out.print(text[i]);
             }
         }
-
+        }
     }
-}
+
+
+
