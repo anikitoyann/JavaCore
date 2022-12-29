3c93fe44ed10e3c8b422083edf11be160f24ec80
Ani <anikitoyan11@gamil.com>
2022-12-29 17:55:58 +0400

    ForIfElseWhileDoWhileForeach

---
+++ src/chapter5/Dowhile.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int n=10;
+        do {
+            System.out.println("takt" + " " + n);
+
+        }
+        while(--n>0);
+
+
+        }
+    }
---
+++ src/chapter5/ForTick.java
@@ -0,0 +1,8 @@
+package chapter5;
+
+public class ForTick {
+    public static void main(String[] args) {
+        for(int n=10;n>0;n--)
+            System.out.println("takt"+" "+n);
+    }
+}
