4086a8413eaedac052cbdfb42071cbb12ce109e8
Ani <anikitoyan11@gamil.com>
2023-02-16 11:52:43 +0400

    EmploeeDate

--- src/homeworks/employee/EmployeeDemo.java
+++ src/homeworks/employee/EmployeeDemo.java
@@ -146,7 +146,6 @@ public class EmployeeDemo {
         String[] emploeeData = emploeeDataStr.split(",");
         String id = emploeeData[2];
         Date data=new Date();
-        String dateStr=sdf.format(data);
         String myDataStr=emploeeData[6];
         Date dataOfBirthday=sdf.parse(myDataStr);
         Employee employeeById = employeStorage.getEmplooyebyID(id);
