09d2250cd927666e060fcb7c1689adab982030bb
Ani <anikitoyan11@gamil.com>
2023-03-11 16:58:53 +0400

    collections

--- src/homeworks/collection/CollectionExample2.java
+++ src/homeworks/collection/CollectionExample2.java
@@ -34,13 +34,22 @@ public class CollectionExample2 {
         try {
             String sortname = scanner.nextLine();
             String[] sortNameData = sortname.split(",");
-            Collections.addAll(sortName, sortNameData);
+            sortName.add(sortNameData[0]);
+            sortName.add(sortNameData[1]);
+            sortName.add(sortNameData[2]);
+            sortName.add(sortNameData[3]);
+            sortName.add(sortNameData[4]);
+            sortName.add(sortNameData[5]);
+            sortName.add(sortNameData[6]);
+            sortName.add(sortNameData[7]);
+            sortName.add(sortNameData[8]);
+            sortName.add(sortNameData[9]);
         } catch (ArrayIndexOutOfBoundsException exc) {
             System.out.println("Incorrect number of element!!!Please try agayin");
             return;
         }
         for (String s : sortName) {
-            System.out.print(s+" ");
+            System.out.print(s + " ");
         }
     }
 }
